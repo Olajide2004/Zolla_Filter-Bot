134c073a82479e42a0fb36e092ce433fcdca0a05
Olajide2004 <116316884+Olajide2004@users.noreply.github.com>
2024-11-17 11:04:09 +0100

    Update index.py

--- plugins/index.py
+++ plugins/index.py
@@ -1,6 +1,6 @@
-# Don't Remove Credit @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Don't Remove Credit @olajidesherif
+# Subscribe YouTube Channel For Amazing Bot @osunhive
+# Ask Doubt on telegram @olajidesherif
 
 import logging, re, asyncio
 from utils import temp
