294d3e3b387651646039d7b32cfe61fabeced998
Olajide2004 <116316884+Olajide2004@users.noreply.github.com>
2024-11-17 11:10:41 +0100

    Update repo.py

--- plugins/Extra/repo.py
+++ plugins/Extra/repo.py
@@ -1,6 +1,6 @@
-# Don't Remove Credit @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Don't Remove Credit @olajidesherif
+# Subscribe YouTube Channel For Amazing Bot @osunhive
+# Ask Doubt on telegram @olajidesherif
 
 
 import logging
@@ -35,7 +35,7 @@ async def git(bot, message):
 
 <b>Open Issues :</b> <i>{qw.get("open_issues")}</i>
 
-<b>Powered by :</b> @VJ_Botz
+<b>Powered by :</b> @olajide2004
 
 """
         if qw.get("description"):
